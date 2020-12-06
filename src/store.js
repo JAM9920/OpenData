5dd1f1419f4a306cf826dbf4bed94f5022a8ee62
j.abdusodikov <j.abdusodikov@osg.uz>
2020-12-06 15:56:55 +0500

    bug

--- src/store.js
+++ src/store.js
@@ -8,20 +8,13 @@ export default new Vuex.Store({
   state: {
     Authenticated: false,
     profileData: {},
-    access_level: -1,
-    user_types: {
-      admin: 0,
-      user: 1
-    }
   },
   mutations: {
     setAuthorization(state, payload) {
       state.Authenticated = payload;
     },
     setProfile(state, payload) {
-      const accessLevel = (payload || {}).user_type
 			state.profileData = payload;
-      state.access_level = accessLevel;
 
       // console.log('profiledata', state.profileData)
 
